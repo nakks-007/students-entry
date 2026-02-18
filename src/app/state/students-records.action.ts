9e36cbcf86505446b01daba28eeade4381e2f653
naga <121928949+nakks-007@users.noreply.github.com>
2026-02-18 06:07:08 +0530

    JSON Server Installed for API Mocking

---
+++ src/app/state/students-records.action.ts
@@ -0,0 +1,10 @@
+import { createAction, props } from "@ngrx/store";
+import { StudentsRecords } from "./students-records";
+
+export const actionsList = {
+    callStudentsRecordsApi: "[ Students Table Component ] Call Students Records API",
+    callStudentsRecordsApiSuccess: "[ Students Table Component ] Call Students Records API Success",
+}
+
+export const callStudentsRecordsApi = createAction(actionsList.callStudentsRecordsApi);
+export const callStudentsRecordsApiSuccess = createAction(actionsList.callStudentsRecordsApiSuccess, props<{ payload: StudentsRecords[] }>());
